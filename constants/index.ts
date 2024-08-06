cae9eb59efefd6192065fdf0fd4aab641b5083d4
shovonroy173 <shovonroy2003@gmail.com>
2024-08-06 14:06:35 +0530

    constants added

---
+++ constants/index.ts
@@ -0,0 +1,390 @@
+export const Skill_data = [
+    {
+      skill_name: "Html 5",
+      Image: "/html.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Css",
+      Image: "/css.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Java Script",
+      Image: "/js.png",
+      width: 65,
+      height: 65,
+    },
+    {
+      skill_name: "Tailwind Css",
+      Image: "/tailwind.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "React",
+      Image: "/react.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Redux",
+      Image: "/redux.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "React Query",
+      Image: "/reactquery.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Type Script",
+      Image: "/ts.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Next js 13",
+      Image: "/next.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Framer Motion",
+      Image: "/framer.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Stripe Payment",
+      Image: "/stripe.webp",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Node js",
+      Image: "/node-js.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Mongo db",
+      Image: "/mongodb.png",
+      width: 40,
+      height: 40,
+    },
+  
+  ];
+  
+  export const Socials = [
+    {
+      name: "Github",
+      src: "/github.png",
+      link:"https://github.com/shovonroy173"
+    },
+    {
+      name: "LinkedIn",
+      src: "/linkedin.png",
+      link:"https://www.linkedin.com/in/shovonroy-mern-developer/"
+    },
+  ];
+  
+  export const Frontend_skill = [
+    
+    {
+      skill_name: "React",
+      Image: "/react.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Redux",
+      Image: "/redux.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "React Query",
+      Image: "/reactquery.svg",
+      width: 80,
+      height: 80,
+    },
+    
+    {
+      skill_name: "Sass",
+      Image: "/sass.png",
+      width: 65,
+      height: 65,
+    },
+    {
+      skill_name: "Tailwind Css",
+      Image: "/tailwind.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Material UI",
+      Image: "/mui.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Html 5",
+      Image: "/html.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Css",
+      Image: "/css.png",
+      width: 80,
+      height: 80,
+    },
+    
+    // {
+    //   skill_name: "Type Script",
+    //   Image: "/ts.png",
+    //   width: 80,
+    //   height: 80,
+    // },
+    
+  ];
+  
+  export const Backend_skill = [
+    {
+      skill_name: "Node js",
+      Image: "/node-js.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Express js",
+      Image: "/express.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Mongo db",
+      Image: "/mongodb.png",
+      width: 40,
+      height: 40,
+    },
+    {
+      skill_name: "Fire base",
+      Image: "/Firebase.png",
+      width: 55,
+      height: 55,
+    },
+    {
+      skill_name: "Mongoose",
+      Image: "/mongoose.png",
+      width: 90,
+      height: 90,
+    },
+    // {
+    //   skill_name: "My SQL",
+    //   Image: "/mysql.png",
+    //   width: 70,
+    //   height: 70,
+    // },
+   
+    // {
+    //   skill_name: "Graphql",
+    //   Image: "/graphql.png",
+    //   width: 80,
+    //   height: 80,
+    // },
+  ];
+  
+  export const Full_stack = [
+    {
+      skill_name: "Next js 13",
+      Image: "/next.png",
+      width: 90,
+      height: 80,
+    },
+   
+    {
+      skill_name: "Shadcn",
+      Image: "/shadcn.png",
+      width: 90,
+      height: 90,
+    },  
+    {
+      skill_name: "Clerk",
+      Image: "/clerk.jpg",
+      width: 90,
+      height: 90,
+    },
+    {
+      skill_name: "Next-auth",
+      Image: "/next-auth.png",
+      width: 90,
+      height: 90,
+    },
+    {
+      skill_name: "Zustand",
+      Image: "/zustand.jpg",
+      width: 90,
+      height: 90,
+    },
+    {
+      skill_name: "Prisma",
+      Image: "/prisma.webp",
+      width: 90,
+      height: 90,
+    },
+    {
+      skill_name: "Convex",
+      Image: "/convex.png",
+      width: 90,
+      height: 90,
+    },
+  
+  ];
+  
+  export const Other_skill = [
+    {
+      skill_name: "Stripe",
+      Image: "/stripe.png",
+      width: 100,
+      height: 100,
+    },
+    {
+      skill_name: "Nodemailer",
+      Image: "/nodemailer.png",
+      width: 100,
+      height: 100,
+    },
+    {
+      skill_name: "Stream",
+      Image: "/stream.png",
+      width: 90,
+      height: 90,
+    },
+    {
+      skill_name: "Toastify",
+      Image: "/toastify.png",
+      width: 90,
+      height: 90,
+    },
+    {
+      skill_name: "Framer",
+      Image: "/framer.png",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "Figma",
+      Image: "/figma.png",
+      width: 55,
+      height: 55,
+    },
+  ];
+  
+  export const Language = [
+    {
+      skill_name: "C",
+      Image: "/c_lang.png",
+      width: 85,
+      height: 85,
+    },
+    {
+      skill_name: "Cpp",
+      Image: "/cpp_lang.jpg",
+      width: 90,
+      height: 90,
+    },
+    {
+      skill_name: "Js",
+      Image: "/js.png",
+      width: 90,
+      height: 90,
+    },
+    {
+      skill_name: "Ts",
+      Image: "/ts.png",
+      width: 90,
+      height: 90,
+    },
+    
+  ];
+
+  export const Deploy = [
+    {
+      skill_name: "Vercel",
+      Image: "/vercel.svg",
+      width: 90,
+      height: 90,
+    },
+    {
+      skill_name: "Render",
+      Image: "/render.jpg",
+      width: 90,
+      height: 90,
+    },
+    {
+      skill_name: "Netlify",
+      Image: "/netlify.png",
+      width: 90,
+      height: 90,
+    },
+    {
+      skill_name: "Git",
+      Image: "/git.png",
+      width: 90,
+      height: 90,
+    },
+    {
+      skill_name: "Github",
+      Image: "/github.png",
+      width: 90,
+      height: 90,
+    },
+  ];
+
+  export const Projects_List= [
+    {
+      project_name: "Twitter-Clone",
+      project_image: "/twitter.png",
+      live_link:"https://nextjs-twitter-clone.vercel.app/" , 
+      frontend:"https://github.com/shovonroy173/saas-nextjs-twitter-clone" , 
+      backend:undefined
+    }, 
+    {
+      project_name: "GigGenius",
+      project_image: "/giggenius.png",
+      live_link:"https://job-media-client.vercel.app/" , 
+      frontend:"https://github.com/shovonroy173/job-media-client" , 
+      backend:"https://github.com/shovonroy173/jobs-media-api"
+    },
+    {
+      project_name: "FilmFusion",
+      project_image: "/filmfusion.png",
+      live_link:"https://movies-media-client.vercel.app/" , 
+      frontend:"https://github.com/shovonroy173/movies-media-client" , 
+      backend:"https://github.com/shovonroy173/movies-media-api"
+    },
+    {
+      project_name: "VidVista",
+      project_image: "/vidvista.png",
+      live_link:"https://youtube-clone-client-sage.vercel.app/signin" , 
+      frontend:"https://github.com/shovonroy173/youtube-clone-client" , 
+      backend:"https://github.com/shovonroy173/youtubeApi"
+    },
+    {
+      project_name: "Zoom-Clone",
+      project_image: "/zustand.jpg",
+      live_link:"https://job-media-client.vercel.app/" , 
+      frontend:"https://github.com/shovonroy173/saas-nextjs-zoom-clone" , 
+      backend:"https://github.com/shovonroy173/jobs-media-api"
+    },
+
+  ]
+  
+  
+  
