aad98a5d65d4f90d86fbd0762f854c96ae35215d
shovonroy173 <shovonroy2003@gmail.com>
2025-02-17 23:27:10 +0600

    final draft: footer update

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -1,41 +1,51 @@
 import React from "react";
-import { RxGithubLogo, RxLinkedinLogo } from "react-icons/rx";
+import { RxGithubLogo, RxLinkedinLogo,  } from "react-icons/rx";
+import { IoMail } from "react-icons/io5";
+import { FaPhoneAlt } from "react-icons/fa";
+
 
 import { FaSlack } from "react-icons/fa";
 import Link from "next/link";
 
 const Footer = () => {
   return (
-      <div className="w-full h-full bg-black text-gray-200 shadow-lg p-14 flex flex-col gap-10">
-        <div className="w-full h-full flex flex-wrap  justify-between gap-10">
-          <div className="flex flex-col">
-            <p className="font-bold text-[16px] text-white">Contact</p>
+      <div className="w-full h-full bg-black text-gray-200 shadow-lg p-14 flex flex-col gap-10 ">
+        <div className="w-full h-full flex flex-wrap  justify-between gap-10 z-20">
+          <div className="flex flex-col gap-2">
+            <p className="font-bold text-md text-white">Contact</p>
             <Link
               href="https://www.linkedin.com/in/shovonroy-mern-developer/"
-              className="text-[15px] flex flex-row items-center my-[15px] gap-2 cursor-pointer"
+              className="text-[15px] flex flex-row items-center   gap-2 cursor-pointer"
             >
               <RxLinkedinLogo />
               <span>Linkedin</span>
             </Link>
 
             <Link
-              href="https://shovonworkspacegroup.slack.com"
-              className="text-[15px]  flex flex-row items-center my-[15px] gap-2 cursor-pointer"
+              href="https://mail.google.com/mail/u/1/?view=cm&fs=1&to=shovonroy2003@gmail.com&tf=1
+"
+              className="text-[15px]  flex flex-row items-center   gap-2 cursor-pointer"
             >
-              <FaSlack />
-              <span>Slack</span>
+              <IoMail />
+              <span>                shovonroy2003@gmail.com
+              </span>
             </Link>
+            <p className="text-[15px]  flex flex-row items-center   gap-2 cursor-pointer"
+            >
+              <FaPhoneAlt />
+              <span>+88 01904167449</span>
+            </p>
           </div>
-          <div className="flex flex-col">
-            <div className="font-bold text-[16px] text-white">All Projects</div>
+          <div className="flex flex-col gap-2">
+            <div className="font-bold text-md text-white">All Projects</div>
 
-            <p className="flex flex-row items-center gap-2 cursor-pointer my-[15px]">
+            <p className="flex flex-row items-center gap-2 cursor-pointer  ">
               <RxGithubLogo />
               <span>Github</span>
             </p>
           </div>
-          <div className="  flex flex-col">
-            <div className="font-bold text-[16px] text-white">About</div>
+          <div className="flex flex-col gap-2">
+            <div className="font-bold text-md text-white">About</div>
 
             <Link
               href="/about-me"
@@ -44,11 +54,6 @@ const Footer = () => {
               Learn about me
             </Link>
 
-            <p className="flex flex-row items-center my-[15px] cursor-pointer">
-              <span className="text-[15px]  ">
-                shovonroy2003@gmail.com
-              </span>
-            </p>
           </div>
         </div>
 
